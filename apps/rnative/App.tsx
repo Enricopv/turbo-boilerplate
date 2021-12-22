a6ee77559a815ad0514bff4ac03264d170c6b0eb
Enrico Valbuena <enrico@valpunk.com>
2021-12-22 16:30:08 -0700

    yarn clean

--- apps/rnative/App.tsx
+++ apps/rnative/App.tsx
@@ -82,7 +82,7 @@ const App = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+            screen and then come back to see your edits. ss
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
