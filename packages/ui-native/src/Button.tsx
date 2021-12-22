f83eac062ac8df6aeb44e9e55ffb66d200f3dbb3
Enrico Valbuena <enrico@valpunk.com>
2021-12-22 13:51:19 -0700

    lodash is working

--- packages/ui-native/src/Button.tsx
+++ packages/ui-native/src/Button.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import {Button as RNButton} from "react-native"
+import { orderBy } from 'lodash'
+
+const people = [{name:"jason", age: 333},{name:"joh", age: 32}]
 
 export const Button = () => {
-  return <RNButton title="Cool Native Buttons "/>;
+  console.log('orderBY', orderBy(people, ['age'], ['asc']))
+  return <RNButton title="Cool Native 23  "/>;
 };
