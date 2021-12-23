89390c8a6f3ed3ffaba34bc32a425ee6dc78d204
Enrico Valbuena <enrico@valpunk.com>
2021-12-23 12:50:37 -0700

    installing packages without headache. Issue was in nohoist ui-native

--- packages/ui-native/src/Button.tsx
+++ packages/ui-native/src/Button.tsx
@@ -6,5 +6,5 @@ const people = [{name:"jason", age: 333},{name:"joh", age: 32}]
 
 export const Button = () => {
   console.log('orderBY', orderBy(people, ['age'], ['asc']))
-  return <RNButton title="Cool Native 123  "/>;
+  return <RNButton title="Cool Native   "/>;
 };
---
+++ packages/ui-native/src/__tests__/index.test.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+describe("CounterButton", () => {
+  it("renders without crashing", () => {
+    const hi = true
+    expect(hi).toBeTruthy()
+
+  });
+});
